8f8f21560aea876bab71c4fdbc583f21e29aa138
OriginPeastar735 <devemugaunipro@gmail.com>
2026-04-01 04:42:33 +0900

    fss
    dasf

---
+++ Assets/Scripts/ChartEditor/Fade.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    [SerializeField] private int num = 0;
+    private SpriteRenderer rend;
+    private float alfa = 0;
+    private bool isHover = false;
+
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EditorManager.instance.currentNote == num)
+        {
+            rend.color = new Color(1f,1f,1f,0.5f);
+        }
+        else
+        {
+            if (isHover)
+            {
+                rend.color  =   new Color(0.5f,0.5f,0.5f,0.3f);
+            }
+            else
+            {
+                rend.color  =   new Color(0.5f,0.5f,0.5f,0f);
+            }
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        isHover = true;
+    }
+
+    void OnMouseExit()
+    {
+        isHover = false;
+    }
+
+    void OnMouseDown()
+    {
+            EditorManager.instance.currentNote = num;
+    }
+}
