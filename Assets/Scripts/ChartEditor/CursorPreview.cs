a7e0d021206ceb1da98a811ed8c3b8bfea77b9e0
OriginPeastar735 <devemugaunipro@gmail.com>
2026-04-16 23:50:57 +0900

    CursorPreviewの追加

---
+++ Assets/Scripts/ChartEditor/CursorPreview.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorPreview : MonoBehaviour
+{
+    Vector3 mousePos = Input.mousePosition;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
