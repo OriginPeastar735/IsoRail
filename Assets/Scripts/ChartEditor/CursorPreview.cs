74bd4efecc97ea38fd145d76c9b56ca7d25d50d6
OriginPeastar735 <devemugaunipro@gmail.com>
2026-04-19 01:34:56 +0900

    カーソル座標

--- Assets/Scripts/ChartEditor/CursorPreview.cs
+++ Assets/Scripts/ChartEditor/CursorPreview.cs
@@ -1,17 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CursorPreview : MonoBehaviour
 {
-    Vector3 mousePos = Input.mousePosition;
+    [SerializeField] private Canvas canvas;
+    private RectTransform canvasRectTransform;
+    public int rail = 4;
+    Vector3 mousePos;
+    public Transform RelativeEmpty;
+    public Camera mainCamera;
+    public GameObject previewPrefab;
+    public int currentBar = 0;
+    public int currentBeat = 4;
+    public int barHeight = 400;
+
     void Start()
     {
         
+
+        canvasRectTransform = canvas.GetComponent<RectTransform>();
+        currentBar = EditorManager.instance.currentBar;
+        currentBeat = EditorManager.instance.currentBeat;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        Vector2 localPoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvasRectTransform,
+            Input.mousePosition,
+            canvas.worldCamera,
+            out localPoint
+        );
+        Debug.Log(localPoint);
+    }
+
+    void UpdatePreview()
     {
         
     }
