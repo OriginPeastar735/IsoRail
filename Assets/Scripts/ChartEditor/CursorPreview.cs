72d64c72729c5e7c5e4f6d3e96255b5478d9b6c2
OriginPeastar735 <devemugaunipro@gmail.com>
2026-04-20 00:23:21 +0900

    プレビューノーツの可視化

--- Assets/Scripts/ChartEditor/CursorPreview.cs
+++ Assets/Scripts/ChartEditor/CursorPreview.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 public class CursorPreview : MonoBehaviour
 {
     [SerializeField] private Canvas canvas;
+    private SpriteRenderer rend;
     private RectTransform canvasRectTransform;
     public int rail = 4;
     Vector3 mousePos;
-    public Transform RelativeEmpty;
+    Vector3 LeftEnd;//レールの左下端
+    public int xRailDiff = 70;
+    public int nextBarDiff = 400;
+    public GameObject LeftEndEmpty;
     public Camera mainCamera;
     public GameObject previewPrefab;
     public int currentBar = 0;
@@ -18,16 +22,16 @@ public class CursorPreview : MonoBehaviour
 
     void Start()
     {
-        
-
         canvasRectTransform = canvas.GetComponent<RectTransform>();
-        currentBar = EditorManager.instance.currentBar;
-        currentBeat = EditorManager.instance.currentBeat;
+        LeftEnd = LeftEndEmpty.transform.localPosition;
+        rend = previewPrefab.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentBar = EditorManager.instance.currentBar;
+        currentBeat = EditorManager.instance.currentBeat;
         Vector2 localPoint;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(
             canvasRectTransform,
@@ -35,7 +39,26 @@ public class CursorPreview : MonoBehaviour
             canvas.worldCamera,
             out localPoint
         );
-        Debug.Log(localPoint);
+        for(int i = 1; i <= 4; i++)
+        {
+            if(localPoint.x >= LeftEnd.x + 70 * (i-1) 
+            && localPoint.x <= LeftEnd.x + 70 * i)
+            {
+                for(int j = 0; j < currentBeat; j++)
+                {
+                    float searchY = LeftEnd.y + (nextBarDiff * (j / (float)currentBeat));
+                    Debug.Log(nextBarDiff);
+                    if(localPoint.y >= searchY - 20 
+                    && localPoint.y <= searchY + 20)
+                    {
+                        Vector3 tmp = previewPrefab.transform.localPosition;
+                        tmp.x = LeftEnd.x + 70 * i -35;
+                        tmp.y = searchY;
+                        previewPrefab.transform.localPosition = tmp;
+                    }
+                }
+            }
+        }
     }
 
     void UpdatePreview()
